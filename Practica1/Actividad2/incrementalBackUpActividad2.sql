0faebc8387e85a2a885ebbe70bb99360317b62ff
Randy Sum <randy26smc@gmail.com>
2024-06-11 22:46:34 -0600

    incrementalBackup de paciente Actividad 2

--- Practica1/Actividad2/incrementalBackUpActividad2.sql
+++ Practica1/Actividad2/incrementalBackUpActividad2.sql
@@ -16,45 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `habitacion`
---
-
-DROP TABLE IF EXISTS `habitacion`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `habitacion` (
-  `idHabitacion` int(11) NOT NULL,
-  `habitacion` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idHabitacion`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `habitacion`
---
-
-LOCK TABLES `habitacion` WRITE;
-/*!40000 ALTER TABLE `habitacion` DISABLE KEYS */;
-INSERT INTO `habitacion` VALUES
-(1,'Sala de examenes 1'),
-(2,'Sala de examenes 2'),
-(3,'Sala de examenes 3'),
-(4,'Sala de examenes 4'),
-(5,'Sala de imagenes 1'),
-(6,'Sala de procedimientos 1'),
-(7,'Sala de procedimientos 2'),
-(8,'Sala de procedimientos 3'),
-(9,'Sala de procedimientos 4'),
-(10,'Recepcion'),
-(11,'Laboratorio'),
-(12,'Estación de revisión 1'),
-(13,'Estación de revisión 2'),
-(14,'Estación de revisión 3'),
-(15,'Estación de revisión 4');
-/*!40000 ALTER TABLE `habitacion` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `paciente`
 --
 
@@ -154275,4 +154236,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-11  2:05:38
+-- Dump completed on 2024-06-11 22:44:57
