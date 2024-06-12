3f23d4ea548c7fff8b66e55c5af8feb03a18e55a
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-06-12 00:31:21 -0600

    Cambios actividad 13

--- Practica1/Actividad1/incrementalBackUpActividad1.sql
+++ Practica1/Actividad1/incrementalBackUpActividad1.sql
@@ -16,33 +16,27 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `log_actividad`
+-- Table structure for table `habitacion`
 --
 
-DROP TABLE IF EXISTS `log_actividad`;
+DROP TABLE IF EXISTS `habitacion`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `log_actividad` (
-  `id_log_actividad` int NOT NULL AUTO_INCREMENT,
-  `timestamp` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `actividad` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `idPaciente` int DEFAULT NULL,
-  `idHabitacion` int DEFAULT NULL,
-  PRIMARY KEY (`id_log_actividad`),
-  KEY `idPaciente` (`idPaciente`),
-  KEY `idHabitacion` (`idHabitacion`),
-  CONSTRAINT `log_actividad_ibfk_1` FOREIGN KEY (`idPaciente`) REFERENCES `paciente` (`idPaciente`),
-  CONSTRAINT `log_actividad_ibfk_2` FOREIGN KEY (`idHabitacion`) REFERENCES `habitacion` (`idHabitacion`)
+CREATE TABLE `habitacion` (
+  `idHabitacion` int NOT NULL,
+  `habitacion` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`idHabitacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `log_actividad`
+-- Dumping data for table `habitacion`
 --
 
-LOCK TABLES `log_actividad` WRITE;
-/*!40000 ALTER TABLE `log_actividad` DISABLE KEYS */;
-/*!40000 ALTER TABLE `log_actividad` ENABLE KEYS */;
+LOCK TABLES `habitacion` WRITE;
+/*!40000 ALTER TABLE `habitacion` DISABLE KEYS */;
+INSERT INTO `habitacion` VALUES (1,'Sala de examenes 1'),(2,'Sala de examenes 2'),(3,'Sala de examenes 3'),(4,'Sala de examenes 4'),(5,'Sala de imagenes 1'),(6,'Sala de procedimientos 1'),(7,'Sala de procedimientos 2'),(8,'Sala de procedimientos 3'),(9,'Sala de procedimientos 4'),(10,'Recepcion'),(11,'Laboratorio'),(12,'Estación de revisión 1'),(13,'Estación de revisión 2'),(14,'Estación de revisión 3'),(15,'Estación de revisión 4');
+/*!40000 ALTER TABLE `habitacion` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -54,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-06-11 22:15:07
+-- Dump completed on 2024-06-12  0:30:55
