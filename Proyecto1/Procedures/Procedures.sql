531f2ed4a5ed0f46e19714b8af010a156d4b8fa3
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-06-16 00:40:59 -0600

    Agregando procedimiento de creacion de roles

--- Proyecto1/Procedures/Procedures.sql
+++ Proyecto1/Procedures/Procedures.sql
@@ -1,26 +1,31 @@
 /* 2 */
-CREATE PROCEDURE PR4 
+CREATE PROCEDURE proyecto1.PR4
     @RolName NVARCHAR(30)
 AS
 BEGIN
     
     BEGIN TRANSACTION;
 
-    BEGIN TRY
-    
-        DECLARE @NewID UNIQUEIDENTIFIER;
-    
-        SET @NewID = NEWID();
-    
-        INSERT INTO BD2.proyecto1.Roles (id, RoleName) 
-        VALUES (@NewID, @RolName);
-
-        COMMIT TRANSACTION;
-    END TRY
-    BEGIN CATCH
+    IF (SELECT COUNT(*) FROM BD2.proyecto1.Roles WHERE RoleName = @RolName) > 0
+    BEGIN
+        PRINT 'Ya existe el Rol';
         ROLLBACK TRANSACTION;
+        RETURN;
+    END
+    
+    IF (@RolName = '')
+    BEGIN
+        PRINT 'El nombre del Rol no puede estar vacio';
+        ROLLBACK TRANSACTION;
+        RETURN;
+    END
 
-        THROW;
-    END CATCH;
+    DECLARE @NewID UNIQUEIDENTIFIER;
+    SET @NewID = NEWID();
+    
+    INSERT INTO BD2.proyecto1.Roles (id, RoleName) 
+    VALUES (@NewID, @RolName);
+
+    COMMIT TRANSACTION;
+    
 END
-
