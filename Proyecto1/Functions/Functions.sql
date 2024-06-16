f810f02d1766778b8250b28a4cbf107e37bdded0
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-06-16 17:22:56 -0600

    Agregando funcion 4

--- Proyecto1/Functions/Functions.sql
+++ Proyecto1/Functions/Functions.sql
@@ -15,6 +15,18 @@ RETURN
         Course.CodCourse = @CodCourse
 );
 
+CREATE FUNCTION proyecto1.F4()
+RETURNS TABLE
+AS
+RETURN 
+(
+    SELECT 
+        Id,
+        Date,
+        Description
+    FROM 
+        BD2.proyecto1.HistoryLog 
+);
 
 
 
