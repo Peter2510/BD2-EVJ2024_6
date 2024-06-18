f58dc97eb4ef0bf192312f2470647511c6f2d342
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-06-18 02:02:32 -0600

    Agregando triggers 1, 2 y 3

---
+++ Proyecto1/Triggers/Triggers.sql
@@ -0,0 +1,89 @@
+-- TABLA Roles
+CREATE OR ALTER TRIGGER proyecto1.Trigger1
+ON proyecto1.Roles
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @Operacion VARCHAR(20);
+    DECLARE @Descripcion VARCHAR(100);
+
+    -- Determinar el tipo de operación
+    IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
+        SET @Operacion = 'UPDATE';
+    ELSE IF EXISTS (SELECT * FROM inserted)
+        SET @Operacion = 'INSERT';
+    ELSE IF EXISTS (SELECT * FROM deleted)
+        SET @Operacion = 'DELETE';
+    ELSE
+        RETURN; -- No debería llegar aquí, pero por seguridad
+
+    -- Lógica para manejar las operaciones en las tablas
+    SET @Descripcion = 'Operación ' + @Operacion + ' exitosa en la tabla Roles';
+
+    -- Insertar el registro en la tabla HistoryLog
+    INSERT INTO proyecto1.HistoryLog ([Date], Description)
+    VALUES (GETDATE(), @Descripcion);
+END;
+
+
+-- TABLA CourseAssignment
+CREATE OR ALTER TRIGGER proyecto1.Trigger2
+ON proyecto1.CourseAssignment
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @Operacion VARCHAR(20);
+    DECLARE @Descripcion VARCHAR(100);
+
+    -- Determinar el tipo de operación
+    IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
+        SET @Operacion = 'UPDATE';
+    ELSE IF EXISTS (SELECT * FROM inserted)
+        SET @Operacion = 'INSERT';
+    ELSE IF EXISTS (SELECT * FROM deleted)
+        SET @Operacion = 'DELETE';
+    ELSE
+        RETURN; -- No debería llegar aquí, pero por seguridad
+
+    -- Lógica para manejar las operaciones en las tablas
+    SET @Descripcion = 'Operación ' + @Operacion + ' exitosa en la tabla CourseAssignment';
+
+    -- Insertar el registro en la tabla HistoryLog
+    INSERT INTO proyecto1.HistoryLog ([Date], Description)
+    VALUES (GETDATE(), @Descripcion);
+END;
+
+
+-- TABLA Notification
+CREATE OR ALTER TRIGGER proyecto1.Trigger3
+ON proyecto1.Notification
+AFTER INSERT, UPDATE, DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @Operacion VARCHAR(20);
+    DECLARE @Descripcion VARCHAR(100);
+
+    -- Determinar el tipo de operación
+    IF EXISTS (SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
+        SET @Operacion = 'UPDATE';
+    ELSE IF EXISTS (SELECT * FROM inserted)
+        SET @Operacion = 'INSERT';
+    ELSE IF EXISTS (SELECT * FROM deleted)
+        SET @Operacion = 'DELETE';
+    ELSE
+        RETURN; -- No debería llegar aquí, pero por seguridad
+
+    -- Lógica para manejar las operaciones en las tablas
+    SET @Descripcion = 'Operación ' + @Operacion + ' exitosa en la tabla Notification';
+
+    -- Insertar el registro en la tabla HistoryLog
+    INSERT INTO proyecto1.HistoryLog ([Date], Description)
+    VALUES (GETDATE(), @Descripcion);
+END;
+
