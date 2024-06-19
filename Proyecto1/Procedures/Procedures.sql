c08b72f10cf6132b5f2cae6796035c98e9a47a7e
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-06-19 01:27:10 -0600

    Agregando infomacion de error a history los PR3 y PR4

--- Proyecto1/Procedures/Procedures.sql
+++ Proyecto1/Procedures/Procedures.sql
@@ -9,16 +9,16 @@ BEGIN
     IF (SELECT COUNT(*) FROM BD2.proyecto1.Roles WHERE RoleName = @RolName) > 0
     BEGIN
         PRINT 'Ya existe el Rol';
-        INSERT INTO BD2.proyecto1.HistoryLog(Description, Date) VALUES ('Operación INSERT errónea en la tabla Roles, ya existe un Rol con el nombre ' + @RolName, GETDATE());
         ROLLBACK TRANSACTION;
+        INSERT INTO BD2.proyecto1.HistoryLog(Description, Date) VALUES ('Operación INSERT errónea en la tabla Roles, ya existe un Rol con el nombre ' + @RolName, GETDATE());
         RETURN;
     END
     
     IF (@RolName = '')
     BEGIN
         PRINT 'El nombre del Rol no puede estar vacio';
-        INSERT INTO BD2.proyecto1.HistoryLog(Description, Date) VALUES ('Operación INSERT errónea en la tabla Roles, el nombre del Rol no puede estar vacio', GETDATE());
         ROLLBACK TRANSACTION;
+        INSERT INTO BD2.proyecto1.HistoryLog(Description, Date) VALUES ('Operación INSERT errónea en la tabla Roles, el nombre del Rol no puede estar vacio', GETDATE());
         RETURN;
     END
 
@@ -30,7 +30,7 @@ BEGIN
     VALUES (@NewID, @RolName);
     SET @Description = 'Operación INSERT exitosa, se ha insertado un nuevo Rol en la tabla Roles, con el nombre ' + @RolName;
     INSERT INTO BD2.proyecto1.HistoryLog(Description, Date) VALUES (@Description, GETDATE());
-
+    PRINT 'Rol ' + @RolName + ' creado exitosamente en la tabla Roles';
     COMMIT TRANSACTION;
     
 END
